51c39b4eef7f468dd4e08b64b522c1adb25db033
Alexander Terp <alexander.terp@gmail.com>
2021-09-19 22:29:25 +1000

    5.2 Remove card back when card is clicked
    Remove the other cards to just focus on one for now.
    
    We add a card back to our tree card and a script for when the card is
    clicked, removing the card back (to reveal the underlying card).

---
+++ unity-in-action-memory/Assets/MemoryCard.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+
+    [SerializeField] private GameObject cardBack;
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
