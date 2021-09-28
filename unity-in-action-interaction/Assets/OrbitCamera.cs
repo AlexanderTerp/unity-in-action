9a252260acbc07e40fa0d66c7c5d893736d36d4c
Alexander Terp <alexander.terp@gmail.com>
2021-09-28 23:01:01 +1000

    Chapter 9: Initial third-person project copy
    This chapter builds off the third person project, so let's commit a copy
    of it to build from.

---
+++ unity-in-action-interaction/Assets/OrbitCamera.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    public float rotationSpeed = 1.5f;
+
+    [SerializeField] private Transform _target = null;
+    private float _rotY;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _offset = _target.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        _rotY += horInput * rotationSpeed;
+        _rotY += Input.GetAxis("Mouse X") * rotationSpeed * 3;
+
+        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        transform.position = _target.position - (rotation * _offset);
+        transform.LookAt(_target);
+    }
+}
